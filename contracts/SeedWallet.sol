d88b0de682aca84fc860340f38f4d1217a025e2e
haidn123 <haidn@relipasoft.com>
2023-06-14 13:04:28 +0700

    seed wallet

--- contracts/SeedWallet.sol
+++ contracts/SeedWallet.sol
@@ -8,75 +8,78 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract SeedWallet is Ownable, ReentrancyGuard {
     struct User {
         uint256 balance;
-
+        bool isClaimTGE;
     }
-
-    uint256 public _min;
-    uint256 public _max;
-    uint256 public _startAt;
-    uint256 public _endAt;
-    uint256 public _cost;
-    uint256 public _balanceTotal;
-    bool public _soldOut;
+    uint256 public cap;
+    uint256 public cost;
+    uint256 public min;
+    uint256 public max;
+    uint256 public totalTokensSold;
+    uint256 public totalBusdRaised;
+    uint256 public startAt;
+    uint256 public endAt;
+    uint256 public tgeStart;
+    uint256 public lockingTime;
+    uint256 public tge;
+    bool public soldOut;
     address public _exchangeToken;
-    address[] private _userKeys;
+    address public _token;
     address[] public whiteList;
 
-    mapping(address => User) public _user;
+    mapping(address => User) public users;
 
     event SoldOut(bool soldout);
     event Buy(address indexed buyer, uint256 amount);
+    event ClaimWDA(address indexed buyer, uint256 amount);
 
-    constructor(address exchangeToken) {
-        _exchangeToken=exchangeToken;
+    constructor(address exchangeToken, address token) {
+        _exchangeToken = exchangeToken;
+        _token = token;
     }
 
     function initialize(
-        uint256 min,
-        uint256 max,
-        uint256 startAt,
-        uint256 endAt,
-        uint256 balanceTotal
+        uint256 _min,
+        uint256 _max,
+        uint256 _startAt,
+        uint256 _endAt,
+        uint256 _cap,
+        uint256 _cost,
+        uint256 _tge
     ) external onlyOwner {
-        require(min <= max, "SEED: Min lower than max");
-        _min = min;
-        _max = max;
-        _startAt = startAt;
-        _endAt=endAt;
-        _balanceTotal = balanceTotal;
+        require(_min <= _max, "SEED: Min lower than max");
+        cap = _cap;
+        min = _min;
+        max = _max;
+        startAt = _startAt;
+        endAt = _endAt;
+        cost = _cost;
+        tge = _tge;
     }
 
     function buy(uint256 amount) external onlyDuring nonReentrant {
-        require(_max > 0, "SEED: Not initial");
-        require(_min <= _max, "SEED: min higer than max");
-        require(amount >= _min, "SEED: min invalid");
-        require(amount<=_max , "SEED: max invalid");
-        require(
-            getTotalBought() + amount <= _balanceTotal,
-            "SEED: Higer than total"
-        );
+        require(max > 0, "SEED: Not initial");
+        require(amount >= min || amount <= max, "SEED:out of bound");
+        require(totalTokensSold + amount <= cap, "SEED: Higer than total");
         require(isWhiteList(msg.sender), "SEED: Address not whitelist");
-        if (!isExistAddress(msg.sender)) {
-            _user[msg.sender] = User(0);
-            _userKeys.push(msg.sender);
+
+        User storage user = users[msg.sender];
+        user.balance += amount;
+
+        if (totalTokensSold == cap) {
+            soldOut = true;
+            emit SoldOut(soldOut);
         }
-
-        User storage user = _user[msg.sender];
-        user.balance+=amount;
-
-        if (getTotalBought() == _balanceTotal) {
-            _soldOut = true;
-            emit SoldOut(_soldOut);
-        }
-
-        uint256 total = (amount * 10 ** 18) * _cost;
-
-        uint256 allowance = IERC20(_exchangeToken).allowance(msg.sender,address(this));
-
-        require(allowance>=total,"SEED:Insufficent allowance");
-
-        IERC20(_exchangeToken).transferFrom(msg.sender,address(this),total);
-
+        uint256 total = amount * cost;
+        require(
+            IERC20(_exchangeToken).transferFrom(
+                msg.sender,
+                address(this),
+                total
+            ),
+            "Token transfer failed"
+        );
+        totalTokensSold += amount;
+        totalBusdRaised += total;
         emit Buy(msg.sender, amount);
     }
 
@@ -89,40 +92,55 @@ contract SeedWallet is Ownable, ReentrancyGuard {
         return false;
     }
 
-    function setWhiteList(address[] calldata users) external onlyOwner{
+    function setWhiteList(address[] calldata _users) external onlyOwner {
         delete whiteList;
-        whiteList=users;
+        whiteList = _users;
     }
 
-    function getTotalBought() public view returns (uint256) {
-        uint256 totalBought = 0;
-        for (uint256 i = 0; i < _userKeys.length; i++) {
-            totalBought += _user[_userKeys[i]].balance;
-        }
-        return totalBought;
+    function getBalanceOfSeed() public view returns (uint256) {
+        return cap - totalTokensSold;
     }
 
-    function getTotalUnbought() public view returns(uint256){
-        return _balanceTotal-getTotalBought();
-    }
-    
-
-    function isExistAddress(address user) public view returns (bool) {
-        for (uint256 i = 0; i < _userKeys.length; i++) {
-            if (_userKeys[i] == user) {
-                return true;
-            }
-        }
-        return false;
+    function withDrawBusd() external onlyOwner {
+        uint256 amount = IERC20(_exchangeToken).balanceOf(address(this));
+        require(IERC20(_exchangeToken).transfer(owner(), amount),"Transfer token failed");
     }
 
+    function setTgeStart(
+        uint256 _tgeStart,
+        uint256 _lockingTime
+    ) external onlyOwner {
+        require(tgeStart >= block.timestamp && tgeStart>=endAt, "SEED: Tge start invalid");
+        tgeStart = _tgeStart;
+        lockingTime = _lockingTime + tgeStart;
+    }
 
+    function claimTGE() external onlyTgeStart nonReentrant {
+        require(block.timestamp >= lockingTime, "SEED: Lock in time");
+        User storage user = users[msg.sender];
+        require(!user.isClaimTGE, "SEED: Already claim tge");
+        uint256 amount = (user.balance * tge) / 100;
+        _claimToken(amount);
+        user.isClaimTGE = true;
+    }
+
+    function _claimToken(uint256 amount) internal {
+        uint256 balanceOfSeed = IERC20(_token).balanceOf(address(this));
+        require(balanceOfSeed >= amount, "Insufficent token");
+        require(IERC20(_token).transfer(msg.sender, amount),"Transfer token failed");
+        emit ClaimWDA(msg.sender, amount);
+    }
 
     modifier onlyDuring() {
-        require(block.timestamp >= _startAt &&block.timestamp <= _endAt , "SEED: Not in time");
+        require(
+            block.timestamp >= startAt && block.timestamp <= endAt,
+            "SEED: Not in time"
+        );
         _;
     }
 
-    // function withdraw
-
+    modifier onlyTgeStart() {
+        require(block.timestamp >= tgeStart, "SEED: early for tge");
+        _;
+    }
 }
